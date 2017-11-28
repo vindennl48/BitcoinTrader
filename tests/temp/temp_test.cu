e5e4cebcb54349b8b587b3924f1a0d5f762e9a5a
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2017-11-28 04:15:09 +0000

    update

--- tests/temp/temp_test.cu
+++ tests/temp/temp_test.cu
@@ -5,29 +5,41 @@
 
 __global__
 void add(int *a, int *b, int *c){
-  *c = *a + *b;
+  c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
 }
 
+#define N 512
+
 int main(){
-  int a, b, c;
+  int *a, *b, *c;
   int *d_a, *d_b, *d_c;
-  int size = sizeof(int);
+  int size = N*sizeof(int);
 
   cudaMalloc((void **)&d_a, size);
   cudaMalloc((void **)&d_b, size);
   cudaMalloc((void **)&d_c, size);
 
-  a = 2;
-  b = 7;
+  a = (int *)malloc(size);
+  b = (int *)malloc(size);
+
+  for(int i=0; i<N; i+=1){
+    a[i] = i;
+    b[i] = i;
+  }
 
   cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
 
-  add<<<1,1>>>(d_a, d_b, d_c);
+  add<<<1,N>>>(d_a, d_b, d_c);
 
-  cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+  free(a);
+  free(b);
+  free(c);
 
   cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+
 
   return 0;
 }
